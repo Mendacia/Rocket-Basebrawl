31b440abda0795808e44a64f65d40bb124432f19
Zach <spywoawe@gmail.com>
2020-09-07 11:42:16 +1200

    Detecting

---
+++ Assets/Scripts/BattingOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattingOnClick : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            HitBall();
+        }
+    }
+
+    void HitBall()
+    {
+        //playAnimation
+        if(Detector.isHittable == true)
+        {
+            //hitball
+        }
+    }
+}
---
+++ Assets/Scripts/Detector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    public static bool isHittable = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        isHittable = true;
+        Debug.Log("This shouldn't be going off");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isHittable = false;
+    }
+}
