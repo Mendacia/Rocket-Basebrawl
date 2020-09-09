212353efc9cdf93208de463d07d212f30754f350
Liam <liamh2000@gmail.com>
2020-09-10 11:58:09 +1200

    Controls

---
+++ Assets/Scripts/Player/Movement/runningPhaseMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runningPhaseMovement : MonoBehaviour
+{
+    [Header("Player Speed")]
+    [SerializeField] private float speed = 1;
+    [Header ("Plug the root camera object in here")]
+    [SerializeField] private Transform cameraRotationReferenceY = null;
+    private Rigidbody rb;
+    
+    //Inputs that I need to carry from Update to FixedUpdate
+    private float movementX = 0;
+    private float movementZ = 0;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        //Taking Inputs
+        movementX = Input.GetAxis("Horizontal") * speed;
+        movementZ = Input.GetAxis("Vertical") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(cameraRotationReferenceY.forward, Vector3.up);
+        rb.velocity = transform.right * movementX + transform.forward * movementZ;
+    }
+}
---
+++ Assets/Scripts/baseCheckpointScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseCheckpointScript : MonoBehaviour
+{
+    [SerializeField] private string PreviousBase = ("Previous Base");
+    [SerializeField] private string thisBase = ("This Base");
+    private string currentBase = ("Home");
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == ("Player"))
+        {
+            
+        }
+    }
+}
