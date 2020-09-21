44e45e43230ddc938d916e2cb62e94b6b53c8fa3
Liam <liamh2000@gmail.com>
2020-09-22 11:52:25 +1200

    UI stuff

---
+++ Assets/Scripts/UI/uIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject rootMenu;
+    [SerializeField] private GameObject newGameMenu;
+    [SerializeField] private GameObject continueMenu;
+
+    public void ToggleRoot()
+    {
+        rootMenu.SetActive(!rootMenu.gameObject.activeSelf);
+    }
+
+    public void ToggleNewGame()
+    {
+        newGameMenu.SetActive(!rootMenu.gameObject.activeSelf);
+    }
+
+    public void ToggleContinue()
+    {
+        continueMenu.SetActive(!continueMenu.gameObject.activeSelf);
+    }
+}
