0c4c39c577dbce58320cb1dd91643b7011f4581c
Liam <liamh2000@gmail.com>
2020-09-23 11:52:50 +1200

    More UI changes

---
+++ Assets/Scripts/UI/onUIButtonLoadScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onUIButtonLoadScene : MonoBehaviour
+{
+    [Header("Make sure this is spelled right")]
+    [SerializeField] private string sceneToLoad = "Change This In Editor";
+
+    private bool hasStayed = false;
+
+    public void loadTheScene()
+    {
+        if (hasStayed)
+        {
+            Debug.LogError("If you're seeing this then the scene name is spelled wrong on '" + gameObject.name + "'");
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneToLoad);
+            hasStayed = true;
+        }
+    }
+}
