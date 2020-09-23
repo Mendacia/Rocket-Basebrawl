30b3666be87116372db5b45048940798b99d6e1f
Liam <liamh2000@gmail.com>
2020-09-24 10:54:24 +1200

    THA MIXTAPE
    AINT NOTHING LIKE A FUNKY BEAT

---
+++ Assets/Scripts/God/Music/godsMixtape.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class godsMixtape : MonoBehaviour
+{
+    [SerializeField] private AudioSource theCamera;
+    [SerializeField] private List<AudioClip> theMixtape;
+    private AudioClip currentSong;
+    private int selectedSongInt = 0;
+
+
+    private void Start()
+    {
+        currentSong = theMixtape[selectedSongInt = Random.Range(0, theMixtape.Count)];
+        theCamera.clip = currentSong;
+        theCamera.Play(0);
+    }
+
+    private void Update()
+    {
+        if (!theCamera.isPlaying)
+        {
+            int newSelectedSongThatCannotBeTheSameAsSelectedSongInt = Random.Range(0, theMixtape.Count);
+            while(newSelectedSongThatCannotBeTheSameAsSelectedSongInt == selectedSongInt)
+            {
+                //IT CANNOT BE THE SAME
+                newSelectedSongThatCannotBeTheSameAsSelectedSongInt = Random.Range(0, theMixtape.Count);
+            }
+            selectedSongInt = newSelectedSongThatCannotBeTheSameAsSelectedSongInt;
+            currentSong = theMixtape[selectedSongInt];
+            theCamera.clip = currentSong;
+            theCamera.Play(0);
+        }
+    }
+}
