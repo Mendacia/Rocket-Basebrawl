75c000f6720ea88e9ee844035880192d35824809
Nemesia <nemesiaphemeral@gmail.com>
2020-10-20 10:41:14 +1300

    Ball hit randomization

--- Assets/Scripts/Fielders/fielderTargetingSuccessfulHit.cs
+++ Assets/Scripts/Fielders/fielderTargetingSuccessfulHit.cs
@@ -9,12 +9,16 @@ public class fielderTargetingSuccessfulHit : MonoBehaviour
 
     public void SpawnTheBaseballPrefabAtThePlayerAndHitItRealHard(Vector3 midPointLocation)
     {
+        //Don't worry about these causing lag, it only runs when the player hits a ball, it shouldn't be that taxing.
         GameObject.Find("Scoreholder").GetComponent<scoreHolder>().score++;
         var myLineRendererScript = gameObject.GetComponent<fielderTargetingLineRenderer>();
+
+        //Following this is just making sure the ball goes in the right direction
         var myBaseballObject = Instantiate(baseballPrefab);
         myBaseballObject.GetComponent<fielderPeltingBallBehaviour>().ballIsActive = false;
         myBaseballObject.transform.position = midPointLocation;
-        myBaseballObject.transform.LookAt(myLineRendererScript.originPosition - myLineRendererScript.direction);
+        myBaseballObject.transform.LookAt(myLineRendererScript.originPosition - myLineRendererScript.direction); //You could change this LookAt to a position the player is looking at if you want
+        myBaseballObject.transform.Rotate((Random.Range(-45, 45)), (Random.Range(-45, 45)), (Random.Range(-45, 45)), Space.Self); //If you do, remove this or change variation or whatever
         myBaseballObject.transform.position = myBaseballObject.transform.position + new Vector3(0, 0, 1);
     }
 }
