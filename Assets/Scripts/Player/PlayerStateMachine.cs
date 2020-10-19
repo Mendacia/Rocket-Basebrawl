1ba9de9cfa373508f55c105388bfeea600bbb8c5
Nemesia <nemesiaphemeral@gmail.com>
2020-10-20 09:31:13 +1300

    Moved a package and added an endscreen script

---
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    public enum playerState
+    {
+        BATTING,
+        RUNNING,
+        CINEMATIC
+    }
+}
