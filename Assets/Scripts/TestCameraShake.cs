86e4f0c381adaab73ddcf2aae055a68faecb695d
Zach <spywoawe@gmail.com>
2020-10-21 11:42:30 +1300

    Camera wall avoidance and Camera shake

---
+++ Assets/Scripts/TestCameraShake.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraShake : MonoBehaviour
+{
+    public CinemachineCameraShake camShake;
+    public CinemachineCameraShake camShakeAim;
+
+    public void StartShake()
+    {
+        camShake.Noise(0.75f, 6);
+        camShakeAim.Noise(0.75f, 6);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            StartCoroutine(TurnShakeOnAndOff());
+        }
+    }
+
+    IEnumerator TurnShakeOnAndOff()
+    {
+        camShake.Noise(0.75f, 6);
+        camShakeAim.Noise(0.75f, 6);
+        yield return new WaitForSeconds(0.3f);
+        camShake.Noise(0, 0);
+        camShakeAim.Noise(0, 0);
+    }
+}
