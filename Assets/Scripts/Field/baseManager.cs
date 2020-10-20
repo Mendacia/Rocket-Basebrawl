d9dc3598cc4c50ef51b0009518acda702eaec6cc
Nemesia <nemesiaphemeral@gmail.com>
2020-10-21 11:57:58 +1300

    Base script rework and beginning on new fielder targeting

---
+++ Assets/Scripts/Field/baseManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class baseManager : MonoBehaviour
+{
+    [Header("You shouldn't need to touch these, they're visible for debug purposes")]
+    public List<Transform> bases = null;
+    public int currentBase = 0;
+    public int nextBase = 1;
+    [Header("Set these to their respective objects please and thank you")]
+    [SerializeField] private Transform playerPosition = null;
+    [SerializeField] private Text uIBaseText = null;
+    [SerializeField] private fielderTargetingRangeAllocator rangeAllocationScript = null;
+    [Header("This is 5 by default")]
+    [SerializeField] private float distanceFromBaseRequiredToProgress = 5;
+    private bool hasLeftHome = false;
+    
+
+    private void Start()
+    {
+        //Populating list, if bases are wack order the prefab better, if bases break in build, this is probably the issue.
+        foreach (Transform child in gameObject.transform)
+        {
+            bases.Add(child);
+        }
+
+        //Sets initial base to home
+        currentBase = 0;
+    }
+
+    private void Update()
+    {
+        //Updates the current base if the player is within the distanceFromBaseRequiredToProgress
+        if(currentBase != (bases.Count -1))
+        {
+            if (Vector3.Distance(playerPosition.position, bases[(nextBase = currentBase + 1)].position) < distanceFromBaseRequiredToProgress)
+            {
+                currentBase = nextBase;
+                rangeAllocationScript.RangeAllocatorNextBaseUpdater();
+                hasLeftHome = true;
+            }
+        }
+        //This is here because it covers an error where if this was an "or" in the above "if" then the next base would be higher than the index of bases, causing an error.
+        else
+        {
+            if (currentBase == (bases.Count - 1) && Vector3.Distance(playerPosition.position, bases[nextBase = 0].position) < distanceFromBaseRequiredToProgress)
+            {
+                currentBase = nextBase;
+                rangeAllocationScript.RangeAllocatorNextBaseUpdater();
+            }
+        }
+
+        //Loads scene on return to home, replace this later
+        if (currentBase == 0 && hasLeftHome)
+        {
+            SceneManager.LoadScene("EndingBasebrawlTestingZone");
+        }
+
+        uIBaseText.text = bases[currentBase].name;
+    }
+}
---
+++ Assets/Scripts/Field/fielderTargetingRangeAllocator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fielderTargetingRangeAllocator : MonoBehaviour
+{
+    [Header("Plug everything in please and thanks")]
+    [SerializeField] private baseManager baseManagerScript;
+    [SerializeField] private Transform playerPosition = null;
+    private Transform recievedNextBase = null;
+
+    public void RangeAllocatorNextBaseUpdater()
+    {
+        recievedNextBase = baseManagerScript.bases[baseManagerScript.nextBase];
+    }
+
+    public void GiveTheFielderATarget()
+    {
+        var nextBaseX = recievedNextBase.position.x;
+        var nextBaseZ = recievedNextBase.position.z;
+        var playerPosX = playerPosition.position.x;
+        var playerPosZ = playerPosition.position.z;
+
+        var finalX = Random.Range(nextBaseX, playerPosX);
+        var finalY = Random.Range(0.2f,3);
+        var finalZ = Random.Range(nextBaseZ, playerPosZ);
+
+        var fielderTarget = new Vector3(finalX, finalY, finalZ);
+    }
+}
