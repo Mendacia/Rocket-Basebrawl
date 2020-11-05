4027e549937ce81e74464eaecf87305693cea00b
Zach <spywoawe@gmail.com>
2020-11-06 09:59:14 +1300

    Changes to prefab + clearer naming

---
+++ Assets/Scripts/Player/EmptyFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyFollowPlayer : MonoBehaviour
+{
+    [Header("This should be the Player Controller")]
+    [SerializeField] private Transform player;
+    [Header("This should be the Main Camera")]
+    [SerializeField] private Transform cameraRotation;
+
+    void Update()
+    {
+        transform.position = player.position;
+        //Rotates player model in direction of the camera
+        transform.eulerAngles = new Vector3(0, cameraRotation.eulerAngles.y, 0);
+    }
+}
---
+++ Assets/Scripts/Player/ModelFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelFollowPlayer : MonoBehaviour
+{
+    [Header("This should be the model")]
+    [SerializeField] private Transform playerModel;
+    [Header("This should be the player")]
+    [SerializeField] private Transform rotationReference;
+
+    void Update()
+    {
+        transform.position = playerModel.position;
+        //Rotates player model in direction of the camera
+        transform.eulerAngles = new Vector3(0, rotationReference.eulerAngles.y + 90, 0);
+    }
+}
