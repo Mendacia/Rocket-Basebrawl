c86453c1d8869dff02c458bea2b4b0b9ae5eb7db
Nemesia <nemesiaphemeral@gmail.com>
2020-12-03 09:02:34 +1300

    Soulja boy tell 'em

---
+++ Assets/Scripts/In Development Scripts/easterEggTester.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class easterEggTester : MonoBehaviour
+{
+    [SerializeField] private GameObject field = null;
+    [SerializeField] private Material woodyMat = null;
+    private enum easterEggState
+    {
+        NA,
+        W,
+        O,
+        O2,
+        D,
+        Y
+    }
+    private easterEggState currentEggState = easterEggState.NA;
+
+    private void Update()
+    {
+        switch (currentEggState)
+        {
+            case easterEggState.NA:
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        currentEggState = easterEggState.W;
+                    }
+                }
+                break;
+            case easterEggState.W:
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown(KeyCode.O))
+                    {
+                        currentEggState = easterEggState.O;
+                    }
+                    else
+                    {
+                        currentEggState = easterEggState.NA;
+                    }
+                }
+
+                break;
+            case easterEggState.O:
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown(KeyCode.O))
+                    {
+                        currentEggState = easterEggState.O2;
+                    }
+                    else
+                    {
+                        currentEggState = easterEggState.NA;
+                    }
+                }
+
+                break;
+            case easterEggState.O2:
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown(KeyCode.D))
+                    {
+                        currentEggState = easterEggState.D;
+                    }
+                    else
+                    {
+                        currentEggState = easterEggState.NA;
+                    }
+                }
+
+                break;
+            case easterEggState.D:
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown(KeyCode.Y))
+                    {
+                        currentEggState = easterEggState.Y;
+                    }
+                    else
+                    {
+                        currentEggState = easterEggState.NA;
+                    }
+                }
+
+                break;
+            case easterEggState.Y:
+                field.GetComponent<MeshRenderer>().material = woodyMat;
+                currentEggState = easterEggState.NA;
+                break;
+        }
+    }
+}
