86c86cbdb9eb67ababe5396f1378ae201c47599c
Nemesia <liamh2000@gmail.com>
2021-03-11 15:02:16 +1300

    Cleaning
    auogh

--- Assets/Scripts/Fielders/fielderPeltingBallBehaviour.cs
+++ Assets/Scripts/Fielders/fielderPeltingBallBehaviour.cs
@@ -4,42 +4,19 @@ using UnityEngine;
 
 public class fielderPeltingBallBehaviour : MonoBehaviour
 {
-    public float ballSpeed, pingSpeed;
-    public bool isHittable = true;
-    public bool ballIsActive = true;
-
-    private scoreUpdater myScoreUpdater;
-    private BaseballEffectHolder effectHolder;
-    private AudioSource pitchChange;
+    [SerializeField] private float ballSpeed;
+    [SerializeField] private float expireAfterSeconds = 20;
 
     void Start()
     {
-        myScoreUpdater = GameObject.Find("ScoreUpdater").GetComponent<scoreUpdater>();
-        effectHolder = GameObject.Find("BaseballEffectHolder").GetComponent<BaseballEffectHolder>();
-
         var body = GetComponent<Rigidbody>();
         body.velocity = transform.forward * ballSpeed * 100 * Time.deltaTime;
         StartCoroutine(Expire());
-
-        pitchChange = Camera.main.GetComponent<AudioSource>();
     }
-
-    //Not being used - Delete?
-    /*private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "BallHit")
-        {
-            var body = GetComponent<Rigidbody>();
-            Vector3 pingDirection = new Vector3(Random.Range(0, -5), Random.Range(0, 2), Random.Range(-5, 5));
-            body.velocity += pingDirection * (ballSpeed / 2) * pingSpeed * Time.deltaTime;
-            isHittable = false;
-            Detector.ballCols.Remove(this.gameObject.transform);
-        }
-    }*/
 
     IEnumerator Expire()
     {
-        yield return new WaitForSeconds(60);
+        yield return new WaitForSeconds(expireAfterSeconds);
         Destroy(gameObject);
     }
 }
--- Assets/_TakeNote/Editor/Core/Main.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FuguFirecracker.TakeNote
-{
-	internal static class Main
-	{
-		internal static Window Window { get; set; }
-	}
-}
