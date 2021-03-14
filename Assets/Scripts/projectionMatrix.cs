564dc22e59fe631cf087bdd11e38b83bb13f0181
Zach <spywoawe@gmail.com>
2021-03-15 09:09:58 +1300

    Push

--- Assets/Scripts/projectionMatrix.cs
+++ Assets/Scripts/projectionMatrix.cs
@@ -5,43 +5,20 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class projectionMatrix : MonoBehaviour
 {
-    public float left = -0.2F;
-    public float right = 0.2F;
-    public float top = 0.2F;
-    public float bottom = -0.2F;
-    void LateUpdate()
+    public Matrix4x4 originalProjection;
+    Camera cam;
+
+    void Start()
     {
-        Camera cam = Camera.main;
-        Matrix4x4 m = PerspectiveOffCenter(left, right, bottom, top, cam.nearClipPlane, cam.farClipPlane);
-        cam.projectionMatrix = m;
+        cam = GetComponent<Camera>();
+        originalProjection = cam.projectionMatrix;
     }
 
-    static Matrix4x4 PerspectiveOffCenter(float left, float right, float bottom, float top, float near, float far)
+    void Update()
     {
-        float x = 2.0F * near / (right - left);
-        float y = 2.0F * near / (top - bottom);
-        float a = (right + left) / (right - left);
-        float b = (top + bottom) / (top - bottom);
-        float c = -(far + near) / (far - near);
-        float d = -(2.0F * far * near) / (far - near);
-        float e = -1.0F;
-        Matrix4x4 m = new Matrix4x4();
-        m[0, 0] = x;
-        m[0, 1] = 0;
-        m[0, 2] = a;
-        m[0, 3] = 0;
-        m[1, 0] = 0;
-        m[1, 1] = y;
-        m[1, 2] = b;
-        m[1, 3] = 0;
-        m[2, 0] = 0;
-        m[2, 1] = 0;
-        m[2, 2] = c;
-        m[2, 3] = d;
-        m[3, 0] = 0;
-        m[3, 1] = 0;
-        m[3, 2] = e;
-        m[3, 3] = 0;
-        return m;
+        Matrix4x4 p = originalProjection;
+        p.m01 += Mathf.Sin(Time.time * 1.2F) * 0.1F;
+        p.m10 += Mathf.Sin(Time.time * 1.5F) * 0.1F;
+        cam.projectionMatrix = p;
     }
 }
