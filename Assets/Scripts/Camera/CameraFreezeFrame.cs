366b6c6e661ebd161d981d7a23b558b6ebad5f29
Zach <spywoawe@gmail.com>
2021-03-26 11:56:18 +1300

    Time scale changes on freeze frame

--- Assets/Scripts/Camera/CameraFreezeFrame.cs
+++ Assets/Scripts/Camera/CameraFreezeFrame.cs
@@ -30,15 +30,19 @@ public class CameraFreezeFrame : MonoBehaviour
         vcam.LookAt = GameObject.Find("Baseball(Clone)").transform;
         //vcam.Follow = GameObject.Find("Baseball(Clone)").transform;
         yield return new WaitForSeconds(0.1f);
-        Time.timeScale = 0.5f;
-        firstBallHit = true;
-        yield return StartCoroutine(WaitForRealSeconds(waitTime));
+        vcam.m_Lens.FieldOfView = 10;
+        Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(0.3f);
+        vcam.m_Lens.FieldOfView = 20;
+        Time.timeScale = 0.2f;
         if (PauseMenu.isPaused == false)
         {
-            Time.timeScale = 1;
-            cameraRoot.SetActive(false);
-            this.gameObject.SetActive(false);
+            Time.timeScale = Mathf.MoveTowards(Time.timeScale, 1, 25 * Time.unscaledDeltaTime);
+            //cameraRoot.SetActive(false);
+            //this.gameObject.SetActive(false);
         }
+        yield return StartCoroutine(WaitForRealSeconds(waitTime));
+        Time.timeScale = 1;
         this.gameObject.SetActive(false);
     }
 
