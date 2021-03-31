a47dd0a7a132479421f0fd06146d5bcafb52a785
Zach <spywoawe@gmail.com>
2021-04-01 09:58:18 +1300

    2nd brain and post processing sucks. Yay layers!

---
+++ Assets/Scripts/Camera/SportsCamFollowZ.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SportsCamFollowZ : MonoBehaviour
+{
+    [SerializeField] private GameObject player = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+    }
+}
