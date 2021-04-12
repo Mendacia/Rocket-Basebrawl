db1fc3a929a3c59f5e35664144463fbc61041848
Nemesia <liamh2000@gmail.com>
2021-04-12 19:24:43 +1200

    Settings & eyes

---
+++ Assets/DisableHighCostObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableHighCostObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private List<GameObject> targets;
+
+    public void ToggleObjects(bool on)
+    {
+        foreach(GameObject target in targets)
+        {
+            target.SetActive(on);
+        }
+    }
+}
